5c5f7df6fa3ea6bcc3a6547c496ab312deec7aa1
liatReuven <liatreuven08@gmail.com>
2024-06-01 18:39:25 +0300

    עדכון פרטים הורים

--- Final_Project/src/components/Parents/EditProfile.jsx
+++ Final_Project/src/components/Parents/EditProfile.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Efooter from '../../Elements/Efooter';
 
 import '../../assets/StyleSheets/EditProfileP.css';
 
 export default function EditProfile() {
+    const [userData, setUserData] = useState({});
+
+
+    useEffect(() => {
+        const storedUser = JSON.parse(sessionStorage.getItem('currentUser'));
+
+        if (storedUser) {
+            setUserData(storedUser);
+        }
+    }, []);
+
     return (
         <div>
             <form className="bootstrap-edit-profile-container">
                 <h2 className="bootstrap-edit-profile-header"> עריכת פרטים</h2>
-                <Link to="/personal-details/or" className="btn btn-primary bootstrap-edit-profile-button">
-                    פרטים אישיים אור
+                <Link to="/EditProfileChild" className="btn btn-primary bootstrap-edit-profile-button">
+                    פרטים אישיים {userData.firstName}
                 </Link>
-                <Link to="/personal-details/ronit-and-itan" className="btn btn-primary bootstrap-edit-profile-button">
-                    פרטים אישיים רונית ואיתן
+                <Link to="/EditProfileParent" className="btn btn-primary bootstrap-edit-profile-button">
+                    פרטים אישיים {userData.firstName}
                 </Link>
             </form>
             {Efooter}
